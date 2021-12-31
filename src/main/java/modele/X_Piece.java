ff118a0692b25588368217a494c258db65a5fe06
CeliaBriaire <91930913+CeliaBriaire@users.noreply.github.com>
2021-12-31 13:51:25 +0100

    Update X_Piece.java

--- src/main/java/modele/X_Piece.java
+++ src/main/java/modele/X_Piece.java
@@ -5,6 +5,7 @@ public class X_Piece extends Piece {
 	//constructeur
 	public X_Piece(int i,int j,int orientation) {
 		super(i,j,orientation);
+		this.type=5;
 		if(orientation==0) {
 			haut=true; 
 			gauche= true; 
