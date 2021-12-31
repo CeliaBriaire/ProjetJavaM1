4c3978b7cdc0b467db64d1eb4ec4d7dd4b50a47c
CeliaBriaire <91930913+CeliaBriaire@users.noreply.github.com>
2021-12-31 13:40:50 +0100

    Update I_Piece.java

--- src/main/java/modele/I_Piece.java
+++ src/main/java/modele/I_Piece.java
@@ -2,9 +2,15 @@ package modele;
 
 public class I_Piece extends Piece{
 	
-	//constructeur
+	//Constructeur
 	public I_Piece (int i,int j,int orientation) {
 		this.type=2;
+		typeIOrientation(orientation);
+	}
+	
+	
+	//Methodes
+	public void typeIOrientation(int orientation) {
 		if(orientation==0) {
 			haut=true; 
 			bas= true;
@@ -32,18 +38,6 @@ public class I_Piece extends Piece{
 	@Override
 	public void setOrientation(int orientation) {
 		super.setOrientation(orientation);
-		if(this.orientation==0) {
-			haut=true; 
-			bas= true;
-			gauche=false;
-			droite=false;
-			unicode="│";
-			linksImage=getClass().getResource("/images/Piece20.png");}
-		else {gauche=true; 
-			droite=true;
-			haut=false;
-			bas=false;
-			unicode="─";
-			linksImage=getClass().getResource("/images/Piece21.png");}
+		typeIOrientation(orientation);
 	}
 }
